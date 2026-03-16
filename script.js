c1a678fe25016679a20252b6b309519f5af50e95
Nandu Prasad <67225899+nanduprasad99@users.noreply.github.com>
2026-03-16 11:39:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,49 @@
 const sheetURL =
-"https://docs.google.com/spreadsheets/d/e/2PACX-1vT9-C6I8cgaQuPAVggcc8osrWEjO7f8vJWC4hVQ83W78NkxrB_8QoBYDEGzWv0ljpMfKRJJTyM31mRF/pubhtml?gid=0&single=true";
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vT9-C6I8cgaQuPAVggcc8osrWEjO7f8vJWC4hVQ83W78NkxrB_8QoBYDEGzWv0ljpMfKRJJTyM31mRF/pub?output=csv";
 
-fetch(sheetURL)
+fetch(sheetURL + "&t=" + new Date().getTime())
 .then(res => res.text())
 .then(data => {
 
 const rows = data.split("\n").slice(1);
 
-let html="";
+let html = "";
 
 rows.forEach((row,i)=>{
 
-const cols=row.split(",");
+const cols = row.split(",");
 
-const avenger=cols[0];
-const from=cols[1];
-const to=cols[2];
-let status=cols[3];
+const avenger = cols[0];
+const from = cols[1];
+const to = cols[2];
 
-if(!status || status.trim()===""){
-status="Pending";
+let status = (cols[3] || "").trim();
+
+if(status !== "Done"){
+status = "Pending";
 }
 
-const className=status.toLowerCase()=="done"
+const className = status === "Done"
 ? "done"
 : "pending";
 
-html+=`
-
+html += `
 <div class="card ${className}" style="animation-delay:${i*0.2}s">
 
 <div class="name">🦸 ${avenger}</div>
 
 <div class="date">From: ${from}</div>
-
 <div class="date">To: ${to}</div>
 
 <div class="status">
-${status=="Done" ? "✅ Paid" : "⏳ Pending"}
+${status === "Done" ? "✅ Paid" : "⏳ Pending"}
 </div>
 
 </div>
-
 `;
 
 });
 
-document.getElementById("cards").innerHTML=html;
+document.getElementById("cards").innerHTML = html;
 
 });
